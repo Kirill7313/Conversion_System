faa00eb40f2bbd8cf80b0c2ef4deebbad75b1e3b
Kirill7313 <73630738+Kirill7313@users.noreply.github.com>
2024-06-20 21:10:00 +0400

    Add files via upload

---
+++ Switch.py
@@ -0,0 +1,24 @@
+match (int(input("Выберите, куда переводим(1 - из двоичной в десятичную, 2 - из десятичной в двоичную): "))):
+    case 1:
+        inp = input("Введите двоичное число: ")[::-1]
+        res = 0
+        for i in range(len(inp)): 
+            if (int(inp[i]) == 1):
+                res += 2 ** i
+        print("Результат: " + str(res))
+    case 2:
+        inp = input("Введите десятичное число: ")
+        bet = int(inp)
+        res = []
+        while (bet != 0):
+            if (bet % 2 == 1):
+                bet -= 1
+                bet /= 2
+                res.append(1)
+            else:
+                bet /= 2
+                res.append(0)
+        res.reverse()
+        print("Результат: ", end='')
+        for i in res:
+            print(i, end='')
